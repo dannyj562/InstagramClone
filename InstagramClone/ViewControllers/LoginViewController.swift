5410d54785aa9d744d59fb05ee53a153c3b76c47
dannyj562 <riveradanny51@gmail.com>
2018-10-02 19:58:54 -0700

    Sign up and sign in users

---
+++ InstagramClone/ViewControllers/LoginViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LoginViewController.swift
+//  InstagramClone
+//
+//  Created by Danny on 10/2/18.
+//  Copyright © 2018 Danny Rivera. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        loginUser()
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        registerUser()
+    }
+    
+    private func registerUser() {
+        let newUser = PFUser()
+        newUser.username = usernameTextField.text
+        newUser.password = passwordTextField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) -> Void in
+            if success {
+                print("Created a user")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    private func loginUser() {
+        PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!) {
+            (user: PFUser?, error: Error?) -> Void in
+            if user != nil {
+                print("You are logged in")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("User logged in failed: \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
